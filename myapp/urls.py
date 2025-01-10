f66097835166cf758c6f5919e98f9e9c80a6b763
Limbureuben <limbureuben4@gmail.com>
2025-01-10 14:16:57 +0300

    myapp changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,6 @@ from .views import upload_product_image
 
 urlpatterns = [
     path('upload-product-image/', upload_product_image, name='upload-product-image'),
+    path('upload-product-image/', upload_product_image, name='upload-product-image'),
 ]
 
