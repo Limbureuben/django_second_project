152cc9beda2a1a2e6c0f44f204cb13d83aca78d1
Limbureuben <limbureuben4@gmail.com>
2025-01-10 14:22:30 +0300

    The url changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,5 @@ from .views import upload_product_image
 
 urlpatterns = [
     path('upload-product-image/', upload_product_image, name='upload-product-image'),
-    path('upload-product-image/', upload_product_image, name='upload-product-image'),
 ]
 
